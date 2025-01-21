73d009443e193933881347bd11039601d2000f58
Sharwan jung kunwar <sharwankunwar07@gmail.com>
2025-01-21 13:46:42 +0545

    added inner shadow in boxes

--- scripts/script.js
+++ scripts/script.js
@@ -2748,7 +2748,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // The new shortcut placeholder info
     const PLACEHOLDER_SHORTCUT_NAME = "New shortcut";
-    const PLACEHOLDER_SHORTCUT_URL = "https://github.com/XengShi/materialYouNewTab";
+    const PLACEHOLDER_SHORTCUT_URL = "ShortCut link past here";
 
     // The placeholder for an empty shortcut
     const SHORTCUT_NAME_PLACEHOLDER = "Shortcut Name";
